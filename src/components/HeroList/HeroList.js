b40b659d842c555d28a635f150ad4e69bbf13e6e
Tomas Sukenik <tomas-sukenik@github.com>
2020-06-20 13:01:10 +0200

    added toast UI from react-toastify package

--- src/components/HeroList/HeroList.js
+++ src/components/HeroList/HeroList.js
@@ -2,8 +2,11 @@ import React, { Component } from "react"
 import Axios from "axios"
 import './HeroList.css'
 
-import Modal from '../UI/Modal/Modal'
+import {toast, ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import HeroDetail from '../HeroDetail/HeroDetail'
+import Modal from '../UI/Modal/Modal'
 
 class heroList extends Component {
   // url = "http://localhost:80/api"
@@ -32,6 +35,7 @@ class heroList extends Component {
         const fetchedHero = response.data
         this.setSelectedHero(fetchedHero)
         this.toggleModalHandler(fetchedHero)
+        this.setToastMessage(fetchedHero.name)
       })
       .catch((err) => { console.log('fetching hero detail failed', err) })
   }
@@ -43,8 +47,12 @@ class heroList extends Component {
 
   toggleModalHandler = () => {
     const isModalOpened = this.state.modalOpened;
-
     this.setState({ modalOpened: !isModalOpened })
+  }
+
+  setToastMessage = (msg) => {
+    //todo Refactor as component if possible
+    toast(msg);
   }
 
   render() {
@@ -56,9 +64,12 @@ class heroList extends Component {
       </ul>
     ) : null;
 
+    toast.configure();
+
 
     return (
       <>
+        <ToastContainer />
         <Modal show={this.state.modalOpened} onClose={this.toggleModalHandler}><HeroDetail name={this.selectedHero.name} id={this.selectedHero.id} /></Modal>
         {heroes}
       </>
