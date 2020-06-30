5d67c1a720492e02f6e572a281210279fc7a5625
Tomas Sukenik <tomas-sukenik@github.com>
2020-06-30 13:48:48 +0200

    implement routing and adjust styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+import NavBar from './components/UI/Navbar/Navbar'
 import HeroList from './components/HeroList/HeroList'
+import PrivatePage from './components/PrivatePage/PrivatePage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HeroList />
+        <BrowserRouter>
+          <NavBar />
+          <Route path="/" exact component={HeroList} />
+          <Route path="/private-page" component={PrivatePage} />
+        </BrowserRouter>
       </div>
     );
   }
